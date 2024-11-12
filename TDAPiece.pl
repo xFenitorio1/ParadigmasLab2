2da63f4b0320eca4c447e2a4527a1cdef3d5f72d
Gerardo Perez <gerardo.perez@usach.cl>
2024-11-12 00:25:42 -0300

    Creacion TDA

---
+++ TDAPiece.pl
@@ -0,0 +1,2 @@
+
+piece(Color, [Color]).
---
+++ TDAPlayer.pl
@@ -0,0 +1,3 @@
+% Constructor para un jugador
+% player(ID, Name, Color, Wins, Losses, Draws, RemainingPieces, Player).
+player(ID, Name, Color, Wins, Losses, Draws, RemainingPieces, [ID, Name, Color, Wins, Losses, Draws, RemainingPieces]).
